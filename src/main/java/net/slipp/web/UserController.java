f6c67921c7e1c85d01d5e6e1735a2422faa123d3
ksm0814 <ksm0814@naver.com>
2018-01-17 16:51:54 +0900

    회원 목록 조회

---
+++ src/main/java/net/slipp/web/UserController.java
@@ -0,0 +1,34 @@
+package net.slipp.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class UserController {
+	private List<User> users = new ArrayList<User>();
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	@GetMapping("/")
+	public String welcomePage() {
+		return "form";
+	}
+	@PostMapping("/create")
+	public String create(User user) {
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users);
+		return "list";
+	}
+	
+}
